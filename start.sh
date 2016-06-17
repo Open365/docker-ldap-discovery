58a28a2d42898377694cdcd1541402a3aa3e3ff4
louis.sanchez <louis.sanchez@eyeos.com>
2016-06-17 11:22:20 +0200

    re-add add_ldif function to start.sh
    · this function is needed as a fail-safe in case the ldapadd operation
    fails because an entry already exists.  the error is ignored and the
    script continues.
    
    · the function was originally removed when migrating to alpine because
    the -c flag should technically force a continue without breaking the
    script, but this doesn't appear to be the case so we are putting it
    back.

--- start.sh
+++ start.sh
@@ -2,6 +2,30 @@
 set -e
 ulimit -n 1024
 
+ERR_ENTRY_ALREADY_EXISTS=68
+add_ldif() {
+	local file="$1"
+	local errcode
+	echo "adding contents of $file"
+	if ldapadd -c -y /tmp/ldap_rootpwd -D "cn=Manager,dc=eyeos,dc=com" -H ldapi:// -f "$file"
+	then
+		# we only need to do something when ldapadd fails, but using 'if ! ldapadd ...' would set
+		# $? to 0, which is useless to us. So we leave the ldapadd normally, do nothing if it
+		# succeeds and if it fails we check if the error code is the expected one in case of
+		# duplicated entries or it is another unexpected one, we fail
+		echo "$file: Added successfully"
+	else
+		errcode="$?"
+		if [ "$errcode" = "$ERR_ENTRY_ALREADY_EXISTS" ]
+		then
+			echo "$file: got duplicated entries, but continuing anyway..."
+		else
+			echo "$file: Failed with errcode $errcode"
+			exit "$errcode"
+		fi
+	fi
+}
+
 cp /tmp/slapd.conf /etc/openldap/slapd.conf
 
 slapd -h "ldap:/// ldapi:///" &
@@ -17,7 +41,7 @@ echo "LDAP is up and running"
 
 SLAPD_PID="$(pgrep slapd)"
 
-ldapadd -c -y /tmp/ldap_rootpwd -D "cn=Manager,dc=eyeos,dc=com" -H ldapi:// -f "/tmp/base.ldif"
+add_ldif /tmp/base.ldif
 
 kill -9 "$SLAPD_PID"
 
